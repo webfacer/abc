3f570cfeac72594c7b261d297cc4ddc541cf05b4
metink <metink@10.211.55.2>
2013-11-07 23:01:32 +0100

    this is a 0.0.1 stable working version with no valid HTML

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,17 +1,3 @@
 $(document).ready(function() {
-	$('h2, p').stickit({
-		useClassName	:'stickit',
-		
-		onEventCopied	: function() {
-			console.log('custom options');
-		},
-		
-		onEventHoverIn	: function() {
-			console.log('class is added!');
-		},
-		
-		onEventHoverOut	: function() {
-			console.log('class is removed');
-		}
-	});
+	$('h2').stickit();
 });
